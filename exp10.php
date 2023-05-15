3fc513b5f377c91ffe970d24b8943a4d8661a4cc
deepshikha2708 <80972038+deepshikha2708@users.noreply.github.com>
2023-05-15 11:56:20 +0530

    Add files via upload

---
+++ exp10.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Test</title>
+	
+</head>
+<body>
+	<header>
+		<h1>Online Test</h1>
+	</header>
+	<main>
+		<form action="submit.php" method="post">
+			<p>
+				<legend>Question 1 :What is the capital of France? </legend>
+				<label for="q1a"><input type="radio" id="q1a" name="q1" value="a"> A : Mumbai</label>
+				<label for="q1b"><input type="radio" id="q1b" name="q1" value="b"> B : Paris</label>
+				<label for="q1c"><input type="radio" id="q1c" name="q1" value="c"> C : Tokyo</label>
+</p>
+			<p>
+				<legend>Question 2: When did India achieve Independence?</legend>
+				<label for="q2a"><input type="radio" id="q2a" name="q2" value="a"> A : 1947 </label>
+				<label for="q2b"><input type="radio" id="q2b" name="q2" value="b"> B : 1963</label>
+				<label for="q2c"><input type="radio" id="q2c" name="q2" value="c"> C : 1924 </label>
+</p>
+			<p>
+				<legend>Question 3: When is World Computer Literacy Day celebrated?</legend>
+				<label for="q3a"><input type="radio" id="q3a" name="q3" value="a"> A : 8th May</label>
+				<label for="q3b"><input type="radio" id="q3b" name="q3" value="b"> B : 2nd December </label>
+				<label for="q3c"><input type="radio" id="q3c" name="q3" value="c"> C : 19th September</label>
+</p>
+
+			<button type="submit">Submit</button>
+		</form>
+	</main>
+	<footer>
+		
+	</footer>
+</body>
+</html>
